057ef6ef4cbd6914195fd96808157fe267e3f28b
flexi0n <github.flx.account@flexion.ch>
2026-03-14 12:47:47 +0000

    exclude sgug from rpath

--- IRIX/configure_irix_local_SDL2-GL.sh
+++ IRIX/configure_irix_local_SDL2-GL.sh
@@ -1,4 +1,5 @@
 echo "Do not forget to run ./bootstrap first"
+echo "This will use libSDL2 with GL support in ./lib or package flx-SDL2 in /usr/sgug-flx/lib32"
 echo "Running configure script"
 
-../configure --disable-x86_64 CFLAGS="-Wl,-rpath,'./lib:/usr/sgug-flx/lib32:/usr/sgug/lib32' -Wl,--allow-shlib-undefined -I/usr/sgug/lib/gcc/mips-sgi-irix6.5/9/include -I/usr/include -I/usr/sgug/include" GL_LIBS="-lGLcore -lGL -lm ./lib/libSDL2.so"
+../configure --disable-x86_64 CFLAGS="-Wl,-rpath,'./lib:/usr/sgug-flx/lib32' -Wl,--allow-shlib-undefined -I/usr/sgug/lib/gcc/mips-sgi-irix6.5/9/include -I/usr/include -I/usr/sgug/include" GL_LIBS="-lGLcore -lGL -lm ./lib/libSDL2.so"
